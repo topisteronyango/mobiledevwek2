30b58d260f66cfe4762057eb2b112dcbad955d77
Lievin <l.niyoyandi@alustudent.com>
2023-01-29 22:39:37 +0200

    Floating action button, side menu, popup menu

--- Container&Padding ConstrainedBox SizedBox/lib/main.dart
+++ Container&Padding ConstrainedBox SizedBox/lib/main.dart
@@ -6,7 +6,10 @@ The goal for this work is to understand:
 */
 
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:flutter/widgets.dart';
+import './about.dart';
+import './logout.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,12 +19,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // Application name
+      debugShowCheckedModeBanner: false,
       title: 'Flutter First Assignment',
-      // color: Color(0xffe32f22),
-      // theme: ThemeData(
-      // primarySwatch: Colors.red,
-      // ),
-      // A widget which will be started on application startup
       home: MyHomePage(title: 'CANVAS'),
     );
   }
@@ -34,83 +33,167 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-        centerTitle: true,
-        backgroundColor: Color(0xff3b3939),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 120,
-              height: 40,
-              color: Color(0xffc69390),
-              padding: EdgeInsets.fromLTRB(20, 10, 25, 5),
-              child: (Text("Dashboard")),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              width: 120,
-              height: 40,
-              color: Color(0xffc69390),
-              padding: EdgeInsets.fromLTRB(30, 10, 30, 5),
-              child: (Text("Courses")),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              width: 120,
-              height: 40,
-              color: Color(0xffc69390),
-              padding: EdgeInsets.fromLTRB(30, 10, 30, 5),
-              child: (Text("Calender")),
-            ),
-            SizedBox(
-              height: 10,
-            ),
+        appBar: AppBar(
+            // The title text which will be shown on the action bar
+            title: Text(title),
+            centerTitle: true,
+            backgroundColor: Colors.blue,
+            actions: <Widget>[
+              PopupOptionMenu(),
+            ]),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 120,
+                height: 40,
+                color: Color(0xffc69390),
+                padding: EdgeInsets.fromLTRB(20, 10, 25, 5),
+                child: (Text("Dashboard")),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: 120,
+                height: 40,
+                color: Color(0xffc69390),
+                padding: EdgeInsets.fromLTRB(30, 10, 30, 5),
+                child: (Text("Courses")),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: 120,
+                height: 40,
+                color: Color(0xffc69390),
+                padding: EdgeInsets.fromLTRB(30, 10, 30, 5),
+                child: (Text("Calender")),
+              ),
+              SizedBox(
+                height: 10,
+              ),
 
-            /*for constrainedBox the size you declared must first check the constraints
+              /*for constrainedBox the size you declared must first check the constraints
             for example the following two container have the same height and width values but they have 
             different constraints that why one is followed the defined values of height and width but 
             the second one followed the minimum constraints values */
-            Container(
-              constraints: BoxConstraints(
-                maxHeight: 100,
-                maxWidth: 200,
-                minHeight: 35,
-                minWidth: 70,
+              Container(
+                constraints: BoxConstraints(
+                  maxHeight: 100,
+                  maxWidth: 200,
+                  minHeight: 35,
+                  minWidth: 70,
+                ),
+                width: 120,
+                height: 40,
+                color: Color(0xffc69390),
+                padding: EdgeInsets.fromLTRB(30, 10, 30, 5),
+                child: (Text("Groups")),
               ),
-              width: 120,
-              height: 40,
-              color: Color(0xffc69390),
-              padding: EdgeInsets.fromLTRB(30, 10, 30, 5),
-              child: (Text("Groups")),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              constraints: BoxConstraints(
-                maxHeight: 30,
-                maxWidth: 80,
-                minHeight: 20,
-                minWidth: 70,
+              SizedBox(
+                height: 10,
               ),
-              width: 120,
-              height: 40,
-              color: Color(0xffc69390),
-              padding: EdgeInsets.fromLTRB(30, 10, 30, 5),
-              child: (Text("Groups")),
-            ),
-          ],
+              Container(
+                constraints: BoxConstraints(
+                  maxHeight: 30,
+                  maxWidth: 80,
+                  minHeight: 20,
+                  minWidth: 70,
+                ),
+                width: 120,
+                height: 40,
+                color: Color(0xffc69390),
+                padding: EdgeInsets.fromLTRB(30, 10, 30, 5),
+                child: (Text("Groups")),
+              ),
+            ],
+          ),
         ),
-      ),
+
+        //floating button named chat with message icon
+        floatingActionButton: FloatingActionButton.extended(
+          // child: Icon(Icons.add),
+          onPressed: () {
+            print("chat activated");
+          },
+          icon: Icon(Icons.message),
+          label: Text("Chat"),
+          foregroundColor: Colors.blue,
+          backgroundColor: Colors.white,
+        ),
+
+        //sidebar navigation menu 
+        drawer: new Drawer(
+          child: ListView(
+            children: <Widget>[
+              new UserAccountsDrawerHeader(
+                accountName: new Text("Lievin"),
+                accountEmail: new Text("niyoyandika@gmail.com"),
+                currentAccountPicture: new CircleAvatar(
+                  backgroundImage: new NetworkImage('http://i.pravator.cc/300'),
+                ),
+              ),
+              new ListTile(
+                title: new Text('About Page'),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                      builder: (BuildContext context) => new AboutPage(),
+                    ),
+                  );
+                },
+              ),
+              new Divider(
+                color: Colors.black,
+                height: 5.0,
+              ),
+              new ListTile(
+                title: new Text('Logout'),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.push(
+                    context,
+                    new MaterialPageRoute(
+                      builder: (BuildContext context) => new LogoutPage(),
+                    ),
+                  );
+                },
+              ),
+              //  PopupOptionMenu(),
+            ],
+          ),
+        ));
+  }
+}
+// popup option menu for page cancel and notifications
+enum MenuOption { Notifications, Discard }
+
+class PopupOptionMenu extends StatelessWidget {
+  const PopupOptionMenu({key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<MenuOption>(
+      itemBuilder: (BuildContext context) {
+        return <PopupMenuEntry<MenuOption>>[      
+          PopupMenuItem(
+            
+            // notification pop up
+            child: Icon(Icons.notifications, color: Colors.black, size: 28),
+            value: MenuOption.Notifications,
+          ),
+          PopupMenuItem(
+            
+            //cancel icon popup
+            child: Icon(Icons.cancel, color: Colors.black, size: 28),
+            value: MenuOption.Discard,
+          ),
+        ];
+      },
     );
   }
 }
